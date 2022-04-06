bb2ec10db1999c6a57ed35293a12bf78e667eaf6
Scotterpop73 <sctzone@aol.com>
2022-04-06 08:47:28 -0700

    Addes some styles to StoriesComponent

--- Components/StoriesComponents.tsx
+++ Components/StoriesComponents.tsx
@@ -3,24 +3,30 @@ import { ScrollView, Text, StyleSheet, View, Image } from "react-native";
 import icon from '../assets/favicon.png';
 const StoriesComponent: FC = () => {
   return (
-    <ScrollView horizontal style={{ 
-        paddingTop: 5, 
-        paddingBottom: 5, 
-        backgroundColor: 'black' 
-    }}>
-      <Image source={icon} style={{height: 50, width: 50, borderRadius: 50, marginRight: 20}} />
-      <Image source={icon} style={{height: 50, width: 50, borderRadius: 50, marginRight: 20}} />
-      <Image source={icon} style={{height: 50, width: 50, borderRadius: 50, marginRight: 20}} />
-      <Image source={icon} style={{height: 50, width: 50, borderRadius: 50, marginRight: 20}} />
-      <Image source={icon} style={{height: 50, width: 50, borderRadius: 50, marginRight: 20}} />
-      <Image source={icon} style={{height: 50, width: 50, borderRadius: 50, marginRight: 20}} />
-      <Image source={icon} style={{height: 50, width: 50, borderRadius: 50, marginRight: 20}} />
+    <ScrollView horizontal style={styles.ScrollStyle}>
+      <Image source={icon} style={styles.ImageStyle} />
+      <Image source={icon} style={styles.ImageStyle} />
+      <Image source={icon} style={styles.ImageStyle} />
+      <Image source={icon} style={styles.ImageStyle} />
+      <Image source={icon} style={styles.ImageStyle} />
+      <Image source={icon} style={styles.ImageStyle} />
+      <Image source={icon} style={styles.ImageStyle} />
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-
+  ImageStyle: {
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    marginRight: 20,
+  },
+  ScrollStyle: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: 'black',
+  },
 });
 
 export default StoriesComponent;
